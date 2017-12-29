3ad54b9801b546c81b2d808a69bade4055dbef05
santiago.correa.dev <santiago.correa.dev@gmail.com>
2017-12-29 11:57:50 -0500

    Added accesibility , additional styling and Service Worker bugs were fixed

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-//Register Service Worker and cache files to be offline
+let staticCacheName = "static-v3";
 self.addEventListener("install", event => {
   //Upload Service Workers faster
   self.skipWaiting();
@@ -15,7 +15,7 @@ self.addEventListener("install", event => {
   ];
   // Cache all files and add a no-cors to get google maps with no issues
   event.waitUntil(
-    caches.open("static-v2").then(cache => {
+    caches.open(staticCacheName).then(cache => {
       return cache.addAll(
         urlsToCache.map(url => {
           return new Request(url, { mode: "no-cors" });
@@ -26,7 +26,18 @@ self.addEventListener("install", event => {
 });
 //Get most updated cached info
 self.addEventListener("activate", function(event) {
-  event.waitUntil(caches.delete("static-v1"));
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys => {
+        return Promise.all(
+          keys
+            .filter(key => !key.startsWith("static"))
+            .map(key => caches.delete(key))
+        );
+      })
+      .then(() => self.clients.claim())
+  );
 });
 //Return files that are cached or through network if network is available
 self.addEventListener("fetch", function(event) {
@@ -35,7 +46,7 @@ self.addEventListener("fetch", function(event) {
       return (
         resp ||
         fetch(event.request).then(function(response) {
-          return caches.open("static-v2").then(function(cache) {
+          return caches.open(staticCacheName).then(function(cache) {
             cache.put(event.request, response.clone());
             return response;
           });
